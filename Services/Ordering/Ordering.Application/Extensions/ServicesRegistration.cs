0f58d13b9b0f0e08f37271c402d8e315960844d2
Luis M. Zotelo <zotelolm@gmail.com>
2024-01-08 20:46:55 -0600

    Add MediatR, FluentValidation and AutoMapper to service registration
    Expanded the service registration in "Ordering.Application" with the addition of auto-mapper configurations, mediator pattern support, and validator registration from executing assembly. This change also includes the transient registration of validation behaviour and unhandled exception behaviour pipeline.

--- Services/Ordering/Ordering.Application/Extensions/ServicesRegistration.cs
+++ Services/Ordering/Ordering.Application/Extensions/ServicesRegistration.cs
@@ -1,4 +1,8 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviour;
 
 namespace Ordering.Application.Extensions;
 
@@ -6,6 +10,11 @@ public static class ServicesRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
         return services;
     }
 }
